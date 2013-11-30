9d12b0eb5fc66bd51a1ff8dbada5dfad4b0d32bf
GleitonFranco <gleitonfranco@gmail.com>
2013-11-30 18:34:13 -0300

    list para cursos e turnos

--- src/main/java/br/edu/fanor/servidores/Flanelografo.java
+++ src/main/java/br/edu/fanor/servidores/Flanelografo.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
@@ -22,38 +23,40 @@ public class Flanelografo implements Serializable {
 	private static final long serialVersionUID = -6186742489880036386L;
 	private static Flanelografo instance;
 	public List<Disciplina> lista;
-	public List<String> lista_cursos;
+	public List<String> listaCursos;
+	public List<String> listaTurnos;
+	public List<String> listaDias;
 	
 	private Flanelografo() {
-		lista_cursos = new ArrayList<String>();
-		lista_cursos.add("");
-        lista_cursos.add("Análise e Des. de Sistemas");
-		lista_cursos.add("Design de Moda");
-		lista_cursos.add("Design Gráfico");
-		lista_cursos.add("Gastronomia");
-		lista_cursos.add("Gestão de TI");
-		lista_cursos.add("Redes de Computadores");
-		lista_cursos.add("Administração");
-		lista_cursos.add("Arquitetura e Urbanismo");
-		lista_cursos.add("Ciências Contábeis");
-		lista_cursos.add("Design");
-		lista_cursos.add("Direito");
-		lista_cursos.add("Educação Física");
-		lista_cursos.add("Enfermagem");
-		lista_cursos.add("Engenharia Ambiental e Sanitária");
-		lista_cursos.add("Engenharia Civil");
-		lista_cursos.add("Engenharia de Produção");
-		lista_cursos.add("Engenharia Elétrica");
-		lista_cursos.add("Engenharia Mecânica");
-		lista_cursos.add("Engenharia Química");
-		lista_cursos.add("Fisioterapia");
-		lista_cursos.add("Jornalismo");
-		lista_cursos.add("Nutrição");
-		lista_cursos.add("Psicologia");
-		lista_cursos.add("Publicidade e Propaganda");
-		lista_cursos.add("Rádio, TV e Internet");
-		lista_cursos.add("Sistemas de Informação");
-		lista_cursos.add("Turismo");
+		listaCursos = new ArrayList<String>();
+		listaCursos.add("");
+        listaCursos.add("Análise e Des. de Sistemas");
+		listaCursos.add("Design de Moda");
+		listaCursos.add("Design Gráfico");
+		listaCursos.add("Gastronomia");
+		listaCursos.add("Gestão de TI");
+		listaCursos.add("Redes de Computadores");
+		listaCursos.add("Administração");
+		listaCursos.add("Arquitetura e Urbanismo");
+		listaCursos.add("Ciências Contábeis");
+		listaCursos.add("Design");
+		listaCursos.add("Direito");
+		listaCursos.add("Educação Física");
+		listaCursos.add("Enfermagem");
+		listaCursos.add("Engenharia Ambiental e Sanitária");
+		listaCursos.add("Engenharia Civil");
+		listaCursos.add("Engenharia de Produção");
+		listaCursos.add("Engenharia Elétrica");
+		listaCursos.add("Engenharia Mecânica");
+		listaCursos.add("Engenharia Química");
+		listaCursos.add("Fisioterapia");
+		listaCursos.add("Jornalismo");
+		listaCursos.add("Nutrição");
+		listaCursos.add("Psicologia");
+		listaCursos.add("Publicidade e Propaganda");
+		listaCursos.add("Rádio, TV e Internet");
+		listaCursos.add("Sistemas de Informação");
+		listaCursos.add("Turismo");
 		
 		lista = new ArrayList<Disciplina>();
 		lista.add(new Disciplina("Sistemas de Informação","Algoritmos","110","Gustavo","M","TER","1"));
@@ -62,6 +65,10 @@ public class Flanelografo implements Serializable {
 		lista.add(new Disciplina("Análise e Des. de Sistemas","Programação Web","Lab.Inf-1","Patrick","N","QUI","4"));
 		lista.add(new Disciplina("Análise e Des. de Sistemas","Topicos ADS","200","Josenio","N","QUI","5"));
 		lista.add(new Disciplina("Análise e Des. de Sistemas","Estatística","250","Pardal","N","SEX","3"));
+		
+		listaTurnos = Arrays.asList(new String[]{"M","T","N"});
+		
+		listaDias = Arrays.asList(new String[]{"SEG","TER","QUA","QUI","SEX"});
 
 	}
 	
@@ -74,7 +81,7 @@ public class Flanelografo implements Serializable {
 	}
 	
 	public String consultaSala(String numeroCurso) {
-		String curso = lista_cursos.get(Integer.parseInt(numeroCurso));
+		String curso = listaCursos.get(Integer.parseInt(numeroCurso));
 		String resposta = "";
 		for (Disciplina d : lista) {
 			if (d.curso.equals(curso)) resposta += d.nome+" "+d.sala+"\n";
